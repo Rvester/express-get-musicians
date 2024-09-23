f595dfd3b465c07a28c1e1fb9e1762598f5d424e
unknown <rasheem.khan94@gmail.comgit config --global user.name Rasheem>
2024-09-23 12:13:12 -0400

    This is fun!

--- index.test.js
+++ index.test.js
@@ -1,22 +1,19 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("return all musicians", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.data).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+});
